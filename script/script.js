c0ce9aff32a9afc7b95444d33efe65aa40ea10aa
NovriMulia <novri.mulia123455@gmail.com>
2026-04-07 23:44:36 +0700

    Init Web Personalia

---
+++ script/script.js
@@ -0,0 +1,55 @@
+const canvas = document.getElementById("matrix");
+const ctx = canvas.getContext("2d");
+
+function resize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resize();
+window.addEventListener("resize", resize);
+
+const letters = "アァカサタナハマヤャラワン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const chars = letters.split("");
+
+const fontSize = 16;
+let columns = canvas.width / fontSize;
+
+let drops = [];
+for (let i = 0; i < columns; i++) {
+  drops[i] = Math.random() * canvas.height;
+}
+
+function draw() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.01)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "#00ff41";
+  ctx.font = fontSize + "px monospace";
+
+  for (let i = 0; i < drops.length; i++) {
+    const text = chars[Math.floor(Math.random() * chars.length)];
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+    if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+      drops[i] = 0;
+    }
+
+    drops[i]++;
+  }
+}
+
+const interval = setInterval(draw, 50);
+
+/* HIDE SPLASH */
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("splash").style.opacity = "0";
+    document.getElementById("main-content").style.opacity = "1";
+
+    setTimeout(() => {
+      document.getElementById("splash").style.display = "none";
+      clearInterval(interval);
+    }, 200);
+
+  }, 800); // durasi splash
+});
